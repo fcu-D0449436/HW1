5e78c12b8a9bf27f8338486736da664a08afc1ba
user <user@FH10>
2016-09-30 09:46:45 +0800

    Change "it" to "her"

--- src/first.java
+++ src/first.java
@@ -2,7 +2,7 @@
 public class first {
 
 	public static void main(String[] args) {
-			String lyrics ="Let it go! Let it go! Cannot hold it back anymore.";
+			String lyrics ="Let her go! Let her go! Cannot hold her back anymore.";
 	}
 
 }
